90ce75d5e630268c551b7e518736e7ad8c12024e
levanhoang07 <levanhoang742004@gmail.com>
2024-04-20 09:53:01 +0700

    lession01

---
+++ Lessione01_LVH/Lessione01_lvh/Lessione01_lvh/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lessione01_lvh.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.name = "Lê Văn Hoàng -K22CNT3";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
